9237d6342c5b65c9f20cc9691b9675045bf9f68b
raphatatto <raphaelatatto@gmail.com>
2024-08-07 20:01:45 -0300

    array commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,14 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List compras = new ArrayList<>();
+        List<String> compras = new ArrayList<>();
+        compras.add("Arroz");
+        compras.add("Feijão");
+        //compras.add(100); erro de compilação por causa do generics "<>"
+
+        //Percorrer a lista
+        for (String item : compras){
+            System.out.println(item);
+        }
     }
 }
